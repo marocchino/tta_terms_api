4a12237845e7cc30c0825cc894aa8bce48874269
Shim Tw <marocchino@gmail.com>
2014-03-03 20:56:14 +0900

    Extract list method

--- lib/tta_terms_api.rb
+++ lib/tta_terms_api.rb
@@ -1,38 +1,13 @@
-require "tta_terms_api/version"
-require "tta_terms_api/word_criteria"
-require "tta_terms_api/word"
 require "tta_terms_api/html"
-
+require "tta_terms_api/version"
+require "tta_terms_api/word"
+require "tta_terms_api/word_criteria"
 module TtaTermsApi
-
   def self.list(options)
-    pattern = %r{sendData\(\s*
-      '(?<gubun>.*)',\s*
-      '(?<terms_num>.*)',\s*
-      '(?<title>.*)',\s*
-      '(?<title_gubun>.*)',\s*
-      '(?<gubun2>.*)',\s*
-      '(?<terms_num2>.*)',\s*
-      '(?<title2>.*)',\s*
-      '(?<title2_gubun>.*)',\s*
-      '(?<gabora_gubun>.*)',\s*
-      '(?<popular>[^']*)'\s*\)
-    }x
-    key = "list-#{options[:search]}"
-    html = HTML.get(:list, key, options)
-    trs = html.css("#tableWidth tr")
-    trs.pop
-    trs.map do |tr|
-      script = tr.css(".input_size0").attr("onkeydown").value
-      match_options = pattern.match(script)
-      options = Hash[ match_options.names.zip( match_options.captures ) ]
-      WordCriteria.new options
-    end
+    WordCriteria.list(options)
   end
 
   def self.view(options)
     WordCriteria.new(options).to_word
   end
-
 end
-
--- lib/tta_terms_api/word_criteria.rb
+++ lib/tta_terms_api/word_criteria.rb
@@ -1,5 +1,30 @@
 module TtaTermsApi
   class WordCriteria < Struct.new(:options)
+    def self.list(options)
+      pattern = %r{sendData\(\s*
+        '(?<gubun>.*)',\s*
+        '(?<terms_num>.*)',\s*
+        '(?<title>.*)',\s*
+        '(?<title_gubun>.*)',\s*
+        '(?<gubun2>.*)',\s*
+        '(?<terms_num2>.*)',\s*
+        '(?<title2>.*)',\s*
+        '(?<title2_gubun>.*)',\s*
+        '(?<gabora_gubun>.*)',\s*
+        '(?<popular>[^']*)'\s*\)
+      }x
+      key = "list-#{options[:search]}"
+      html = HTML.get(:list, key, options)
+      trs = html.css("#tableWidth tr")
+      trs.pop
+      trs.map do |tr|
+        script = tr.css(".input_size0").attr("onkeydown").value
+        match_options = pattern.match(script)
+        options = Hash[ match_options.names.zip( match_options.captures ) ]
+        WordCriteria.new options
+      end
+    end
+
     def to_word
       key = "view-#{options[:gubun]}-#{options[:terms_num]}"
       name, origin, type, similar, description = HTML.get(:view, key, options).css("#printSpace font").text.gsub("\t", "").split(/\n/)
